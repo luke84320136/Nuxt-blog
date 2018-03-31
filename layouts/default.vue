6356b3c88ef47fe6c316fe40e9c51276d3a36b21
lukelan <luke84320136@gmail.com>
2018-04-01 01:59:40 +0800

    Add Lato font-family

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: "Lato", sans-serif;
+  }
+</style>
---
+++ pages/about/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <section id="about-page">
+    <h1>Hello 歡迎光臨</h1>
+    <p>歡迎來到Nuxt Blog</p>
+  </section>
+</template>
+
+<style>
+
+</style>
