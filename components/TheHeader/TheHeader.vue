a1fe87be520533fcf1b586fc79eda9135d5de6cd
lukelan <luke84320136@gmail.com>
2018-04-05 00:31:11 +0800

    Add TheHeader style

---
+++ components/TheHeader/TheHeader.vue
@@ -0,0 +1,58 @@
+<template>
+  <header class="main-header">
+    <nav class="main-nav">
+      <ul class="nav-links">
+        <nuxt-link to="/" tag="li" class="nav-link">
+          <a>全部</a>
+        </nuxt-link>
+        <nuxt-link to="/about" tag="li" class="nav-link">
+          <a>關於</a>
+        </nuxt-link>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  .main-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    background: #022d30;
+    height: 4.5rem;
+  }
+
+  .main-nav {
+    height: 100%;
+  }
+
+  .nav-links {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  .nav-link {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: .1rem;
+    padding: .3rem;
+  }
+
+  .nav-link a {
+    display: block;
+    text-decoration: none;
+    color: white;
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    <nuxt/>
+    <TheHeader />
+    <main class="main-content">
+      <nuxt/>
+    </main>
   </div>
 </template>
 
+<script>
+  import TheHeader from '@/components/TheHeader/TheHeader'
+
+  export default {
+    components: {
+      TheHeader
+    }
+  }
+</script>
 <style>
   * {
     box-sizing: border-box;
@@ -11,5 +23,9 @@
 
   body {
     font-family: "Lato", sans-serif;
+  }
+
+  .main-content {
+    margin-top: 4.5rem;
   }
 </style>
